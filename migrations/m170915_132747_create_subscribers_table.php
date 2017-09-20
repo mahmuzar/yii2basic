ed7a1d166e16872009a43f02cc812210483edd61
murad0587 <myrad_morskoe@mail.ru>
2017-09-20 22:49:33 +0300

    user_id внешний ключ и добавил индекс и данные по умолчанию

--- migrations/m170915_132747_create_subscribers_table.php
+++ migrations/m170915_132747_create_subscribers_table.php
@@ -14,8 +14,21 @@ class m170915_132747_create_subscribers_table extends Migration
     {
         $this->createTable('subscribers', [
             'id' => $this->primaryKey(),
-            'user_id'=> $this->integer()->notNull(),
+            'user_id'=> $this->integer()->notNull()->unique(),
         ]);
+        $this->addForeignKey(
+            'fk-subscribers-user_id',
+            'subscribers',
+            'user_id',
+            'users',
+            'id',
+            'CASCADE'
+        );
+        $this->createIndex(
+            'idx-subscribers-user_id',
+            'subscribers',
+            'user_id'
+        );
          $this->insert('subscribers', array(
             'user_id' => 1,
             
