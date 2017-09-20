405efe22e6c7b1335fa51112e0e8aecfaca0b106
murad0587 <myrad_morskoe@mail.ru>
2017-09-20 23:05:45 +0300

    Шаблон для новых новостей

--- mail/new_news.php
+++ mail/new_news.php
@@ -5,5 +5,5 @@ use yii\helpers\Url;
 <h1>Здравствуйте. Добавлена новая новость!</h1>
 
 <p>Чтобы прочитать перейдите по 
-    <a href="<?= Url::to('@example') ?><?= Url::to(['news/view', 'id' => $event->sender->id]) ?>">ссылке</a>
+    <a href="<?= Url::to('@example') ?><?= Url::to(['news/view', 'id' => $event->getAttribute('id')]) ?>">ссылке</a>
 </p>
