27de253e3461fc8df99facaf8efbba43666e7fc5
murad0587 <myrad_morskoe@mail.ru>
2017-09-20 22:59:10 +0300

    внешние ключи и индексы для всех столбцом кроме id И данные по умеолчанию.

--- migrations/m170915_133152_create_sbr_id_and_notif_opt_id_table.php
+++ migrations/m170915_133152_create_sbr_id_and_notif_opt_id_table.php
@@ -16,6 +16,32 @@ class m170915_133152_create_sbr_id_and_notif_opt_id_table extends Migration {
             'subscriber_id' => $this->integer()->notNull(),
             'notification_option_id' => $this->integer()->notNull()
         ]);
+        $this->addForeignKey(
+            'fk-sbr_id_and_notif_opt_id-subscriber_id',
+            'sbr_id_and_notif_opt_id',
+            'subscriber_id',
+            'subscribers',
+            'id',
+            'CASCADE'
+        );
+        $this->addForeignKey(
+            'fk-sbr_id_and_notif_opt_id-notification_option_id',
+            'sbr_id_and_notif_opt_id',
+            'notification_option_id',
+            'notification_options',
+            'id',
+            'CASCADE'
+        );
+        $this->createIndex(
+            'idx-sbr_id_and_notif_opt_id-notification_option_id',
+            'sbr_id_and_notif_opt_id',
+            'subscriber_id'
+        );
+        $this->createIndex(
+            'idx-sbr_id_and_notif_opt_id-subscriber_id',
+            'sbr_id_and_notif_opt_id',
+            'notification_option_id'
+        );
         $this->insert('sbr_id_and_notif_opt_id', array(
             'subscriber_id' => 1,
             'notification_option_id' => 1,
